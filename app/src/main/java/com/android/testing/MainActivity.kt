6e5f7b8fd4acd3089378f75b4f274908693d0e40
Alen <alen.cerimagic92@gmail.com>
2021-09-23 13:09:04 +0200

    fourth commit

--- app/src/main/java/com/android/testing/MainActivity.kt
+++ app/src/main/java/com/android/testing/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is a new commit change!")
-        shit happens
+
+        asdasdasd
     }
 }
